ce4da6fb67ed28939dde9b0ed2a96d0771ffd9ac
Sergey Vitkovskiy <sergey.vitkovskiy@thepaydock.com>
2017-01-26 19:00:26 +0200

    Name Calculator

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Serg on 26-Jan-17.
+ */
+(function () {
+  'use strict'
+
+
+  angular.module('NameCalculator', [])
+
+    .controller('NameCalculatorController', function ($scope) {
+      $scope.name = '';
+      $scope.totalValue = 0;
+
+    });
+
+})();
